43881147ccd23483750af4ad3cac95235fd0ed33
Snakeshader1 <theo.stoffelbach@epitech.digital>
2022-06-04 21:13:05 +0200

    Auth beginning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./style.css";
 import Main from "./components/Main";
-// import Test3 from "./components/Test3";
+import { useState } from "react";
+import { hasAuth } from "./services/AuthApi";
+import Auth from "./contexts/auth";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(hasAuth());
   return (
-    <div>
-      <Main />
-      {/* <Test3 /> */}
-    </div>
+    <Auth.Provider value={{ isAuth }}>
+      {/* It for route use */}
+
+      <div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Auth.Provider>
   );
 }
 
---
+++ src/services/AuthApi.js
@@ -0,0 +1,3 @@
+export function hasAuth() {
+  return false;
+}
