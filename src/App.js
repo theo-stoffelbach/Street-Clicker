4c990430bef60d5a7be0e521683cdab157567905
Snakeshader1 <theo.stoffelbach@epitech.digital>
2022-06-13 14:37:31 +0200

    fix rooter dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import "./style.css";
 import Main from "./components/Main";
 import { useState } from "react";
-import { hasAuth } from "./services/AuthApi";
-import Auth from "./contexts/auth";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [isAuth, setIsAuth] = useState(hasAuth());
   return (
-    <Auth.Provider value={{ isAuth }}>
-      {/* It for route use */}
-
-      <div>
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </div>
-    </Auth.Provider>
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </div>
   );
 }
 
