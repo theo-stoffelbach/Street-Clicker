1b8055a54f1a9d5828574ca44b1f56a69f983aa5
Snakeshader1 <theo.stoffelbach@epitech.digital>
2022-06-13 17:00:55 +0200

    Update app.js

--- Back/app.js
+++ Back/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 
-// const mongoose = require("mongoose");
-// mongoose.connect(
-//   "mongodb+srv://fs:FloriantetsonJs...@cluster0.fbppb.mongodb.net/Kitty"
-// );
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://fs:FloriantetsonJs...@cluster0.fbppb.mongodb.net/Kitty",
+  {
+    useNewUrlParser: true,
+    UseUnifiedTopology: true,
+  }).;
 
-// const Cat = mongoose.model("Cat", { name: String });
+const Cat = mongoose.model("Cat", { name: String });
 
-// const kitty = new Cat({ name: "Zildjian" });
-// kitty.save().then(() => console.log("meow"));
+const kitty = new Cat({ name: "Zildjian" });
+kitty.save().then(() => console.log("meow"));
 
 module.exports = app;
