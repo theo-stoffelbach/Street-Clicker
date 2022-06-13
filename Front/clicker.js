552c145f97fe6e6b1d9d164d77091e3ebdf15eea
Florian <74573533+UtokaSan@users.noreply.github.com>
2022-06-13 23:28:58 +0200

    Update clicker.js

--- Front/clicker.js
+++ Front/clicker.js
@@ -2,12 +2,24 @@ console.log("JS Launched");
 
 const lifeText = document.querySelector(".life");
 const clickerButton = document.querySelector("#btn_clicker");
+const scoreP = document.querySelector(".score")
 var totalLifeMonster = 100;
-var lifeMonster = totalLifeMonster;
+var result = 0;
+var lifeMonster = 100; 
+var calculPercentage = 0
+
+function clicker () {
+    lifeMonster++;
+    result = lifeMonster;
+    calculPercentage = Math.round(totalLifeMonster / lifeMonster  * 100) + "%";
+    if(calculPercentage < 0) {
+
+    }
+    console.log(calculPercentage)
+    lifeText.style.width = calculPercentage;
+}
 
 clickerButton.addEventListener("click", function () {
-  lifeText.setAttribute("style", "");
-  lifeMonster--;
-  result = lifeMonster;
-  console.log(lifeMonster);
+  clicker();
+  scoreP.innerHTML = result;
 });
